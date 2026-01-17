85a50caeddd751d795713ef557f7b20896e48850
DamianNS <53456589+DamianNS@users.noreply.github.com>
2026-01-17 04:41:04 -0300

    refactor: Renombra métodos de evento a PascalCase, mejora la nulabilidad de propiedades del mapa e inicializa colecciones.

--- Editor/Editor/Models/Mapa.cs
+++ Editor/Editor/Models/Mapa.cs
@@ -2,16 +2,16 @@
 {
     public class Mapa
     {
-        public string Nombre { get; set; }
+        public string? Nombre { get; set; }
         public int Width { get; set; }
         public int Height { get; set; }
-        public Tile[,] Tiles { get; set; }
+        public Tile?[,] Tiles { get; set; }
         public Mapa(int width, int height)
         {
             Width = width;
             Height = height;
-            Tiles = new Tile[width, height];
+            Tiles = new Tile?[width, height];
         }
-        public Dictionary<int,Tile> ExampleTiles { get; set; } = new Dictionary<int,Tile>();
+        public Dictionary<int, Tile> ExampleTiles { get; set; } = new Dictionary<int, Tile>();
     }
 }
